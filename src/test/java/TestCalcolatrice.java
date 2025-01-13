1efcaed5b94f636a48a2918f934bb8bbdc282cb7
DavideVulpes <s8901853u@majorana-giorgi.edu.it>
2025-01-13 09:50:04 +0100

    update

--- src/test/java/TestCalcolatrice.java
+++ src/test/java/TestCalcolatrice.java
@@ -20,8 +20,58 @@ class TestCalcolatrice {
 	{
 		Calcolatrice c = new Calcolatrice();
 		
-		double res = c.somma(6,6);
+		double res = c.somma(2,0);
 		
-		Assert.assertEquals(11,res,0);
+		Assert.assertEquals(2,res,0);
+	}
+	
+	@Test
+	public void testSomma3() 
+	{
+		Calcolatrice c = new Calcolatrice();
+		
+		double res = c.somma(2,-1);
+		
+		Assert.assertEquals(1,res,0);
+	}
+	
+	@Test
+	public void testDifferenza() 
+	{
+		Calcolatrice c = new Calcolatrice();
+		
+		double res = c.differenza(5,2);
+		
+		Assert.assertEquals(3,res,0);
+	}
+	
+	@Test
+	public void testMoltiplicazione() 
+	{
+		Calcolatrice c = new Calcolatrice();
+		
+		double res = c.moltiplicazione(5,2);
+		
+		Assert.assertEquals(10,res,0);
+	}
+	
+	@Test
+	public void testDivisione() 
+	{
+		Calcolatrice c = new Calcolatrice();
+		
+		double res = c.divisione(10,2);
+		
+		Assert.assertEquals(5,res,0);
+	}
+	
+	@Test
+	public void testDivisione2() 
+	{
+		Calcolatrice c = new Calcolatrice();
+		
+		double res = c.divisione(10,0);
+		
+		Assert.assertEquals(10,res,0);
 	}
 }
